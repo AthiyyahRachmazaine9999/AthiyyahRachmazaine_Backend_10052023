459bd180a0bf800c580cd3d5db115e9ae97ec33e
AthiyyahRachmazaine9999 <atya89999@gmail.com>
2023-05-10 17:33:32 +0700

    banksxgxg

---
+++ contact.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Contact Us</title>
+        
+        <link rel="stylesheet" href="newcss.css">
+        <style>
+            .heading{
+    font-weight:bold;
+    color:#2E4372;
+}
+        </style>
+       
+    </head>
+        <?php include 'header.php' ?>
+        <div class='content_customer'>
+            <h3 style="text-align:center;color:#2E4372;"><u>Contact Us</u></h3>
+            
+            <div class="contact">
+            <h3 style="color:#2E4372;"><u>Kansas Branch</u></h3>
+            <p><span class="heading">Address - </span>Globsyn Tower, Kansas.</p>
+            <p><span class="heading">Tel - </span>033-456892/12</p>
+            <p><span class="heading">Email - </span>Kansasbranch@onlinebank.com</p>
+            
+             <h3 style="color:#2E4372;"><u>Oregon Branch</u></h3>
+            <p><span class="heading">Address - </span>Globsyn Tower, Oregon.</p>
+            <p><span class="heading">Tel - </span>013-456856/32</p>
+            <p><span class="heading">Email - </span>Oregonbranch@onlinebank.com</p>
+            
+             <h3 style="color:#2E4372;"><u>Tennessee Branch</u></h3>
+            <p><span class="heading">Address - </span>Globsyn Buisness School, Tennessee.</p>
+            <p><span class="heading">Tel - </span>022-456854/11</p>
+            <p><span class="heading">Email - </span>Tennesseebranch@onlinebank.com</p>
+            </div>
+            </div>
